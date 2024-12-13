83f2c7078e2185accb81baf0af999f8eb42a3f1f
seongjungkim <sjkim@tpcg.co.kr>
2024-12-13 16:36:59 +0900

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 import os
 import json
 
+import dialogflow_agent
+
 def main():
     # Get the input from the GitHub Action
     name = os.getenv("INPUT_NAME", "World")
@@ -8,6 +10,9 @@ def main():
     # Generate a greeting message
     greeting = f"Hello, {name}!"
     print(greeting)
+
+    name = "projects/qnacom-service/locations/global/agents/86920b8a-744d-4e24-8b2d-6d9a6ba8aac6"
+    dialogflow_agent.restore_agent(name, git_source="main")
 
     # Set the output
     with open(os.getenv("GITHUB_OUTPUT"), "a") as output_file:
